e85327e3fe5b1a6fde8551f997ffb2db50b98b95
daniellebandong <danielle_bandong@hotmail.com>
2022-03-28 13:34:47 -0600

    installed react router dom to get routing between pages started

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Stock Tracker Application</h1>
+            <p>Please Login</p>
+            
+        </div>
+    );
+};
+
+export default LoginPage;
+
