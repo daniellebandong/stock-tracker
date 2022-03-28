b1231c2583ceceb19e1343328d6070db0234967d
daniellebandong <danielle_bandong@hotmail.com>
2022-03-28 14:25:30 -0600

    created skeleton home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+const HomePage = (props) => {
+    return (
+        <div>
+            <h1>Employee Manager</h1>
+            <p>focus on people and we'll focus on the paperwork</p>
+            <Link className="homeLinks" to="/login">Login</Link>
+            <Link className="homeLinks" to="/register">Register</Link>
+        </div>
+    );
+};
+
+export default HomePage;
