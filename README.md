8bc0a7c870f0333775ef8f6d9356f42e6d05ca85
daniellebandong <46451872+daniellebandong@users.noreply.github.com>
2022-09-09 19:36:06 -0600

    modified loginpage to start onclick handler

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ I had initially built this application in vanilla JavaScript as a part of my sch
 
 The purpose of this application is for a user to sign up for an account which will go through all the basic checks when
 
-##Tech stack used
+##Tech stack/concepts used
+
+React.js
+(Still need database)
+SCSS
+bcrypt(password hashing for security)
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import {Link} from 'react-router-dom';
 
 import FormInput from './../components/forms/FormInput'
@@ -10,15 +10,22 @@ import './../styles/styles.scss'
 // set a boolean that if the log in information is valid redirect to the home page
 // google bcrypt for password security when logging into the page
 
-const LoginPage = () => {
+const LoginPage = (props) => {
+    //part of the verification process
+    const[email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    //const [isValid, setIsValid] = useState(false)
+
+    //handle click method
+    
     return (
         <header>
             <h1>Welcome to the Stock Tracker Application</h1>         
             {/* still will need to add onChange methods to the FormInputs */}
             <form action="">
                 <p>Please Login</p>
-                <FormInput label="Email Address: " type="text"/>
-                <FormInput label="Password: " type="text"/>
+                <FormInput label="Email Address: " type="text" onChange={(e)=> setEmail(e.target.value.trim())}/>
+                <FormInput label="Password: " type="text" onChange={(e)=> setPassword(e.target.value.trim())}/>
                 <Button  label="login"/>
                 <p>Don't have an account? Click <Link className='homeLinks' to='/register'>here</Link> to sign up!</p>
             </form>
